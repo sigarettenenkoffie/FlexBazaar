50514ebca0177c4b3666dec33dc7edcccfc36377
Hannes <derynckhannes@gmail.com>
2024-05-22 16:15:10 +0200

    configure basic config file

--- FlexBazaar/FlexBazaar.IdentityServer/Config.cs
+++ FlexBazaar/FlexBazaar.IdentityServer/Config.cs
@@ -12,9 +12,29 @@ public static class Config
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
-            { };
+            { 
+                new ApiScope(name: "api", displayName: "FlexBazaar.API")
+            };
 
     public static IEnumerable<Client> Clients =>
         new Client[] 
-            { };
+            { 
+                new Client
+                {
+                    ClientId = "web_client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    // TODO: what grantype best to use for this kind of application?
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("flexbazaarisawesome".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api" }
+                }
+            };
 }
